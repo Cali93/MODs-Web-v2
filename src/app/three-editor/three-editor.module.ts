5613fec6080cfd185374103198a125b1b9e45844
Cali93 <c.armut93@outlook.com>
2018-03-06 10:50:55 +0100

    three editor service created

--- src/app/three-editor/three-editor.module.ts
+++ src/app/three-editor/three-editor.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ThreeEditorRoutingModule } from './three-editor-routing.module';
 import { ThreeEditorComponent } from './three-editor/three-editor.component';
 import { LibComponent } from './lib/lib.component';
+import { ThreeEditorService } from '@app/three-editor/three-editor.service';
 
 @NgModule({
   imports: [
@@ -11,6 +12,7 @@ import { LibComponent } from './lib/lib.component';
     ThreeEditorRoutingModule
   ],
   declarations: [ThreeEditorComponent, LibComponent],
-  exports: [ThreeEditorComponent, LibComponent]
+  exports: [ThreeEditorComponent, LibComponent],
+  providers: [ ThreeEditorService ]
 })
 export class ThreeEditorModule { }
